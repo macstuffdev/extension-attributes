32c3579cfa42b2d2185df76f49378959d5c9794f
macstuffdev <90345470+macstuffdev@users.noreply.github.com>
2021-09-09 13:18:32 +0200

    Update jamfProtectStatus.sh

--- jamfProtectStatus.sh
+++ jamfProtectStatus.sh
@@ -3,15 +3,15 @@
 #   Display Name: Jamf Protect Status
 #   Data type: String
 #   Expected results: Protected, Enrolling, Missing Plan, Disconnected, Protect binary does not exist.
- 
+
 # Jamf Protect Location
 jamfProtectBinaryLocation="/usr/local/bin/protectctl"
 
 if [ -f "$jamfProtectBinaryLocation" ]; then
-    jamfProtectStatus=$("$jamfProtectBinaryLocation" info | awk -F 'Status: ' '{print $2}' | xargs)
+	jamfProtectStatus=$("${jamfProtectBinaryLocation}" info | awk -F 'Status: ' '{print $2}' | xargs)
 else
-	jamfProtectTenant="Protect binary does not exist"
+	jamfProtectStatus="Protect binary does not exist"
 fi
 
 # Display the current status
-echo "<result>$jamfProtectStatus</result>"
+echo "<result>${jamfProtectStatus}</result>"

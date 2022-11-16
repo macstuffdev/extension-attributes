d98d21ee31ca1eb5bcf24c08878946edbe46ed9e
macstuffdev <90345470+macstuffdev@users.noreply.github.com>
2022-11-16 11:52:56 +0100

    Update jamfProtectStatus.sh
    Updated to use plist buddy

--- jamfProtectStatus.sh
+++ jamfProtectStatus.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Extension Attribute to display the status of Jamf Protect
 
 # Created by macstuff.dev - Melwin Moeskops 
 #   Display Name: Jamf Protect Status
 #   Data type: String
-#   Expected results: Protected, Enrolling, Missing Plan, Disconnected, Protect binary does not exist.
+#   Expected results: Protected, Enrolling, Missing Plan, Disconnected, Missing CA, Protect binary does not exist.
 
 # Jamf Protect Location
 jamfProtectBinaryLocation="/usr/local/bin/protectctl"
 
-if [ -f "$jamfProtectBinaryLocation" ]; then
-	jamfProtectStatus=$("${jamfProtectBinaryLocation}" info | awk -F 'Status: ' '{print $2}' | xargs)
+if [[ -f "$jamfProtectBinaryLocation" ]]; then
+	plist=$($jamfProtectBinaryLocation info --plist)
+	jamfProtectStatus=$(/usr/libexec/PlistBuddy -c "Print Status" /dev/stdin <<<"$plist")
 else
 	jamfProtectStatus="Protect binary does not exist"
 fi
